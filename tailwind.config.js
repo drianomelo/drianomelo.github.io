56954a0d7a9e4e4ef38932f0ba63e39e7c234054
Adriano Melo <driano@academico.ufs.br>
2024-01-25 23:37:06 -0300

    Feature: Init

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        60: "#FBF3F3",
+      },
+    },
   },
   plugins: [],
-}
-
+};
