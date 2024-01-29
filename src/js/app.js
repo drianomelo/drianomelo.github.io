7a2468a222f9387eec5829c3e10c4faf3c416940
Adriano <adrianosenny@gmail.com>
2024-01-29 16:03:21 -0300

    Feature: Updates

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,74 @@
-const headerButton = document.querySelector('.header__btn');
-const headerIconButton = document.querySelector('.header__btn i');
-const headerNavigation = document.querySelector('.header__navigation');
+const home = document.querySelector(".home");
 
-headerButton.addEventListener('click', () => {
-    headerNavigation.classList.toggle('header__navigation--active');
-    headerIconButton.classList.toggle('fa-bars');
-    headerIconButton.classList.toggle('fa-xmark');
-});
+const widthHome = home.offsetWidth;
+const heighthHome = home.offsetHeight;
+const qtdCircleTopBottom = Math.ceil(widthHome / 96);
+const qtdCircleLeftRight = Math.floor(heighthHome / 96);
+
+for (let i = 0; i < qtdCircleTopBottom; i++) {
+  const circleTop = createCircleTop(`${i * 90}`);
+  const circleBottom = createCircleBottom(`${i * 90}`);
+
+  home.append(circleTop);
+  home.append(circleBottom);
+}
+
+for (let i = 0; i < qtdCircleLeftRight; i++) {
+  const circleLeft = createCircleLeft(`${i * 90}`);
+  const circleRight = createCircleRight(`${i * 90}`);
+
+  home.append(circleLeft);
+  home.append(circleRight);
+}
+
+function createCircleTop(left) {
+  const circle = document.createElement("div");
+
+  circle.classList.add("absolute", "rounded-full", "bg-60");
+
+  circle.style.width = "118px";
+  circle.style.height = "118px";
+  circle.style.top = "-24px";
+  circle.style.left = left;
+
+  return circle;
+}
+
+function createCircleBottom(right) {
+  const circle = document.createElement("div");
+
+  circle.classList.add("absolute", "rounded-full", "bg-60");
+
+  circle.style.width = "118px";
+  circle.style.height = "118px";
+  circle.style.bottom = "-24px";
+  circle.style.right = right;
+
+  return circle;
+}
+
+function createCircleLeft(bottom) {
+  const circle = document.createElement("div");
+
+  circle.classList.add("absolute", "rounded-full", "bg-60");
+
+  circle.style.width = "118px";
+  circle.style.height = "118px";
+  circle.style.left = "-24px";
+  circle.style.bottom = bottom;
+
+  return circle;
+}
+
+function createCircleRight(top) {
+  const circle = document.createElement("div");
+
+  circle.classList.add("absolute", "rounded-full", "bg-60");
+
+  circle.style.width = "118px";
+  circle.style.height = "118px";
+  circle.style.right = "-24px";
+  circle.style.top = top;
+
+  return circle;
+}
